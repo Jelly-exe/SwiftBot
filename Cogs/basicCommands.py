b7777f96383b210fea647d3330c9c1382cb9edc8
Jelly <Jellybob2016@outlook.com>
2021-11-04 15:56:19 +0000

    Add nick

--- Cogs/basicCommands.py
+++ Cogs/basicCommands.py
@@ -22,6 +22,37 @@ class BasicCommands(commands.Cog):
         )
         await context.send(embed=embed)
 
+    @commands.command(name='nick',
+                      description='Set nick',
+                      usage='nick [user] [nick]')
+    async def nick(self, context, user: discord.Member, *, nick):
+        client = self.client
+
+        if user.id == context.author.id:
+            embed = discord.Embed(
+                description='Cannot change your own nickname.',
+                colour=client.config['embed']['colour']
+            )
+
+            await context.send(embed=embed)
+            return
+
+        try:
+            await user.edit(nick=nick)
+            embed = discord.Embed(
+                description='Nickname changed.',
+                colour=client.config['embed']['colour']
+            )
+            embed.set_footer(text=client.config['embed']['footer']['text'], icon_url=client.config['embed']['footer']['url'])
+
+        except:
+            embed = discord.Embed(
+                description='Failed to change nickname.',
+                colour=client.config['embed']['colour']
+            )
+
+        await context.send(embed=embed)
+
 
 def setup(client):
     client.add_cog(BasicCommands(client))
